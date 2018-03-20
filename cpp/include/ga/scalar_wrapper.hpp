693409975729b6c48d643a0c8e18ea615d84b693
Leandro A. F. Fernandes <laffernandes@ic.uff.br>
2018-03-19 21:29:34 -0300

    Pequenos ajustes.

--- cpp/include/ga/scalar_wrapper.hpp
+++ cpp/include/ga/scalar_wrapper.hpp
@@ -1,148 +1,148 @@
 #ifndef __GA_SCALAR_WRAPPER_HPP__
 #define __GA_SCALAR_WRAPPER_HPP__
 
-#define GA_DECLARE_SCALAR_WRAPPER(TYPE) \
+#define GA_DECLARE_SCALAR_WRAPPER(TYPE, SPECIFIER) \
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator+(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator+(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
 		return ga::plus(lhs, rhs); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator+(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
+	SPECIFIER decltype(auto) operator+(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
 		return ga::plus(lhs, rhs); \
 	} \
 	\
-	constexpr decltype(auto) operator+(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator+(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
 		return ga::plus(lhs, rhs); \
 	} \
 	\
-	constexpr decltype(auto) operator+(TYPE const &lhs, ga::detail::empty_expression const &rhs) { \
-		return ga::plus(lhs, rhs); \
-	} \
-	\
-	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator+(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator+(TYPE const &lhs, ga::detail::empty_expression const &rhs) { \
 		return ga::plus(lhs, rhs); \
 	} \
 	\
 	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator+(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
+	SPECIFIER decltype(auto) operator+(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+		return ga::plus(lhs, rhs); \
+	} \
+	\
+	template<ga::default_integral_t Value> \
+	SPECIFIER decltype(auto) operator+(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
 		return ga::plus(lhs, rhs); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator-(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator-(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
 		return ga::minus(lhs, rhs); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator-(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
+	SPECIFIER decltype(auto) operator-(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
 		return ga::minus(lhs, rhs); \
 	} \
 	\
-	constexpr decltype(auto) operator-(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator-(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
 		return ga::minus(lhs, rhs); \
 	} \
 	\
-	constexpr decltype(auto) operator-(TYPE const &lhs, ga::detail::empty_expression const &rhs) { \
-		return ga::minus(lhs, rhs); \
-	} \
-	\
-	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator-(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator-(TYPE const &lhs, ga::detail::empty_expression const &rhs) { \
 		return ga::minus(lhs, rhs); \
 	} \
 	\
 	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator-(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
+	SPECIFIER decltype(auto) operator-(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+		return ga::minus(lhs, rhs); \
+	} \
+	\
+	template<ga::default_integral_t Value> \
+	SPECIFIER decltype(auto) operator-(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
 		return ga::minus(lhs, rhs); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator*(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator*(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
 		return ga::gp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator*(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
+	SPECIFIER decltype(auto) operator*(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
 		return ga::gp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
-	constexpr decltype(auto) operator*(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator*(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
 		return ga::gp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
-	constexpr decltype(auto) operator*(TYPE const &lhs, ga::detail::empty_expression const &rhs) { \
-		return ga::gp(lhs, rhs, ga::euclidean_metric_t()); \
-	} \
-	\
-	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator*(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator*(TYPE const &lhs, ga::detail::empty_expression const &rhs) { \
 		return ga::gp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
 	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator*(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
+	SPECIFIER decltype(auto) operator*(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+		return ga::gp(lhs, rhs, ga::euclidean_metric_t()); \
+	} \
+	\
+	template<ga::default_integral_t Value> \
+	SPECIFIER decltype(auto) operator*(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
 		return ga::gp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator/(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator/(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
 		return ga::igp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator/(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
+	SPECIFIER decltype(auto) operator/(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
 		return ga::igp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
-	constexpr decltype(auto) operator/(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
-		return ga::igp(lhs, rhs, ga::euclidean_metric_t()); \
-	} \
-	\
-	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator/(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator/(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
 		return ga::igp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
 	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator/(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
+	SPECIFIER decltype(auto) operator/(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+		return ga::igp(lhs, rhs, ga::euclidean_metric_t()); \
+	} \
+	\
+	template<ga::default_integral_t Value> \
+	SPECIFIER decltype(auto) operator/(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
 		return ga::igp(lhs, rhs, ga::euclidean_metric_t()); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator^(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator^(ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &lhs, TYPE const &rhs) { \
 		return ga::op(lhs, rhs); \
 	} \
 	\
 	template<class ElementType, class LeftSubtreeType, class RightSubtreeType> \
-	constexpr decltype(auto) operator^(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
+	SPECIFIER decltype(auto) operator^(TYPE const &lhs, ga::detail::expression<ElementType, LeftSubtreeType, RightSubtreeType> const &rhs) { \
 		return ga::op(lhs, rhs); \
 	} \
 	\
-	constexpr decltype(auto) operator^(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator^(ga::detail::empty_expression const &lhs, TYPE const &rhs) { \
 		return ga::op(lhs, rhs); \
 	} \
 	\
-	constexpr decltype(auto) operator^(TYPE const &lhs, ga::detail::empty_expression const &rhs) { \
-		return ga::op(lhs, rhs); \
-	} \
-	\
-	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator^(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+	SPECIFIER decltype(auto) operator^(TYPE const &lhs, ga::detail::empty_expression const &rhs) { \
 		return ga::op(lhs, rhs); \
 	} \
 	\
 	template<ga::default_integral_t Value> \
-	constexpr decltype(auto) operator^(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
+	SPECIFIER decltype(auto) operator^(ga::detail::cvalue<Value> const &lhs, TYPE const &rhs) { \
+		return ga::op(lhs, rhs); \
+	} \
+	\
+	template<ga::default_integral_t Value> \
+	SPECIFIER decltype(auto) operator^(TYPE const &lhs, ga::detail::cvalue<Value> const &rhs) { \
 		return ga::op(lhs, rhs); \
 	}
 
-GA_DECLARE_SCALAR_WRAPPER(std::int16_t)
-GA_DECLARE_SCALAR_WRAPPER(std::int32_t)
-GA_DECLARE_SCALAR_WRAPPER(std::int64_t)
+GA_DECLARE_SCALAR_WRAPPER(std::int16_t, constexpr)
+GA_DECLARE_SCALAR_WRAPPER(std::int32_t, constexpr)
+GA_DECLARE_SCALAR_WRAPPER(std::int64_t, constexpr)
 
-GA_DECLARE_SCALAR_WRAPPER(std::float_t)
-GA_DECLARE_SCALAR_WRAPPER(std::double_t)
+GA_DECLARE_SCALAR_WRAPPER(std::float_t, constexpr)
+GA_DECLARE_SCALAR_WRAPPER(std::double_t, constexpr)
 
 #endif // __GA_SCALAR_WRAPPER_HPP__
